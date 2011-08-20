68ad709473137b93865aaff9cf3bb9a63111d460
Florent Messa <florent.messa@gmail.com>
2011-08-20 16:44:05 +0200

    Add runit service, remove gunicorn fab tasks

--- fabfile.py
+++ fabfile.py
@@ -16,27 +16,11 @@ env.project_name = 'pluggedinlife'
 env.root = "/var/www/%s" % env.project_name
 env.bin_root = os.path.join(env.root, 'bin')
 env.activate = 'source %s' % os.path.join(env.bin_root, 'activate')
-env.gunicorn_pid_root = '/var/run/gunicorn_pluggedinlife.pid'
+env.service_root = '/var/sv/%s' % env.project_name
 
 # local
 ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
 EXCLUDES = ['bin', '_old', 'include', 'lib', '*.psd', '*.ai', '.Python', 'Fonts', 'build']
-
-def start():
-    with cd(env.root):
-        sudo(env.activate + ' && gunicorn -b 127.0.0.1:5000 --pid=%s server:app' % env.gunicorn_pid_root)
-
-def reload():
-    with cd(env.root):
-        sudo('[ -f %(gunicorn_pid_root)s ] && kill -HUP $(cat %(gunicorn_pid_root)s)' % {
-            'gunicorn_pid_root': env.gunicorn_pid_root
-        })
-
-def stop():
-    with cd(env.root):
-        sudo('[ -f %(gunicorn_pid_root)s ] && kill $(cat %(gunicorn_pid_root)s)' % {
-            'gunicorn_pid_root': env.gunicorn_pid_root
-        })
 
 def publish():
     project.rsync_project(
@@ -45,8 +29,15 @@ def publish():
         local_dir=ROOT_PATH + '/',
     )
 
+def prepare_service():
+    sudo('mkdir -p %s' % env.service_root)
+    sudo('ln -s %s %s' % (os.path.join(env.root, 'run'), os.path.join(env.service_root, 'run')))
+    sudo('chmod +x %s' % os.path.join(env.service_root, 'run'))
+    sudo('ln -s %s /etc/service/%s' % (env.service_root, env.project_name))
+
 def deploy():
     publish()
     with cd(env.root):
         run('virtualenv .')
         run(env.activate + ' && pip install -r requirements.txt')
+        prepare_service()
